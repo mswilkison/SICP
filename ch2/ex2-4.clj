1348a1993877711d524706808cd0e6e86d12e1e0
MacLane Wilkison <mwilkison@gmail.com>
2014-02-09 19:17:11 -0500

    exercise 2.4

---
+++ ch2/ex2-4.clj
@@ -0,0 +1,9 @@
+(defn cons [x y]
+  (fn [m]
+    (m x y)))
+
+(defn car [z]
+  (z (fn [p q] p)))
+
+(defn cdr [z]
+  (z (fn [p q] q)))
--- ch2/notes.clj
+++ ch2/notes.clj
@@ -83,3 +83,16 @@ z
 (defn denom [x]
   (let [g (gcd (first x) (last x))]
     (/ (last x) g)))
+
+; 2.1.3 What Is Meant by Data?
+
+;(defn cons [x y]
+; (defn dispatch [m]
+;    (cond (= m 0) x
+;          (= m 1) y
+;          :else (throw (Exception. (str "Argument not 0 or 1 -- CONS" m)))))
+;  dispatch)
+
+(defn car [z] (z 0))
+
+(defn cdr [z] (z 1))
