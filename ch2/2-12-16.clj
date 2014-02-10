02abaf842fb079a24f297c258fe18d20c4185b98
MacLane Wilkison <mwilkison@gmail.com>
2014-02-10 11:56:38 -0500

    exercise 2.13

--- ch2/2-12-16.clj
+++ ch2/2-12-16.clj
@@ -21,3 +21,19 @@
 
 (defn percent [i]
   (* 100 (/ (width i) (center i))))
+
+; Exercise 2.13
+(defn mul-interval [x y]
+  (let [p1 (* (lower-bound x) (lower-bound y))
+        p2 (* (lower-bound x) (upper-bound y))
+        p3 (* (upper-bound x) (lower-bound y))
+        p4 (* (upper-bound x) (upper-bound y))]
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(def a (make-center-percent 1 5))
+(def b (make-center-percent 1 2))
+
+(percent (mul-interval a b))
+; The tolerance of the product of two intervals is approximately
+; the sum of the tolerance's of the two intervals
