78342013d89b3dc7af20ed921670d9474132af60
MacLane Wilkison <mwilkison@gmail.com>
2014-02-21 12:21:32 -0500

    exercise 2.44 up-split

--- ch2/notes.clj
+++ ch2/notes.clj
@@ -413,3 +413,37 @@ one-through-four
              s)))
 
 (permutations (list 1 2 3))
+
+; 2.2.4 Example: A Picture Language
+(comment
+  (def wave2 (beside wave (flip-vert wave)))
+  (def wave4 (below (wave2 wave2)))
+
+  (defn flipped-pairs [painter]
+    (let [painter2 (beside painter (flip-vert painter))]
+      (below painter2 painter2)))
+
+  (def wave4 (flipped-pairs wave))
+
+  (defn right-split [painter n]
+    (if (= n 0)
+      painter
+      (let [smaller (right-split painter (dec n))]
+        (beside painter (below smaller smaller)))))
+
+  (defn corner-split [painter n]
+    (if (= n 0)
+      painter
+      (let [up (up-split painter (dec n))
+            right (right-split painter (dec n))]
+        (let [top-left (beside up up)
+              bottom-right (below right right)
+              corner (corner-split painter (dec n))]
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+  (defn square-limit [painter n]
+    (let [quarter (corner-split painter n)]
+      (let [half (beside (flip-horiz quarter) quarter)]
+        (below (flip-vert half) half))))
+)
