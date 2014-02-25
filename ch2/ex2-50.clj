209626357b1799899245c8c34827b6e91396da1d
MacLane Wilkison <mwilkison@gmail.com>
2014-02-24 16:51:14 -0800

    exercise 2.50 flip-horiz, rotate180, rotate270

---
+++ ch2/ex2-50.clj
@@ -0,0 +1,17 @@
+(defn flip-horiz [painter]
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(defn rotate180 [painter]
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(defn rotate270 [painter]
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
--- ch2/notes.clj
+++ ch2/notes.clj
@@ -530,3 +530,51 @@ one-through-four
           ((frame-coord-map frame) (start-segment segment))
           ((frame-coord-map frame) (end-segment segment))))
        segment-list))))
+
+; Transforming and combining painters
+(defn transform-painter [painter origin corner1 corner2]
+  (fn [frame]
+    (let [m (frame-coord-map frame)]
+      (let [new-origin (m origin)]
+        (painter
+          (make-frame new-origin
+                      (sub-vect (m corner1) new-origin)
+                      (sub-vect (m corner2) new-origin)))))))
+
+(defn flip-vert [painter]
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(defn shrink-to-upper-right [painter]
+  (transform-painter painter
+                     (make-vect 0.5 0.5)
+                     (make-vect 1.0 0.5)
+                     (make-vect 0.5 1.0)))
+
+(defn rotate90 [painter]
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(defn squash-inwards [painter]
+  (transform-painter painter
+                     (make-vect 0.0 0.0)
+                     (make-vect 0.65 0.35)
+                     (make-vect 0.35 0.65)))
+
+(defn beside [painter1 painter2]
+  (let [split-point (make-vect 0.5 0.0)]
+    (let [paint-left (transform-painter painter1
+                                        (make-vect 0.0 0.0)
+                                        split-point
+                                        (make-vect 0.0 1.0))
+          paint-right (transform-painter painter2
+                                         split-point
+                                         (make-vect 1.0 0.0)
+                                         (make-vect 0.5 1.0))]
+      (fn [frame]
+        (paint-left frame)
+        (paint-right frame)))))
