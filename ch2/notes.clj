ed33c4a15647cf1fe5a726531bdbd61d21e7c1c8
MacLane Wilkison <mwilkison@gmail.com>
2014-02-26 12:28:01 -0800

    exercise 2.56

--- ch2/notes.clj
+++ ch2/notes.clj
@@ -604,3 +604,67 @@ one-through-four
 
 (memq 'apple '(pear banana prune))
 (memq 'apple '(x (apple sauce) y apple pear))
+
+; 2.3.2 Example: Symbolic Differentation
+(defn variable? [x]
+  (symbol? x))
+
+(defn same-variable? [v1 v2]
+  (and (variable? v1) (variable? v2)) (= v1 v2))
+
+(defn make-sum [a1 a2]
+  (list '+ a1 a2))
+
+(defn make-product [m1 m2]
+  (list '* m1 m2))
+
+(defn sum? [x]
+  (and (seq? x) (= (first x) '+)))
+
+(defn addend [s] (second s))
+
+(defn augend [s] (last s))
+
+(defn product? [x]
+  (and (seq? x) (= (first x) '*)))
+
+(defn multiplier [p]
+  (second p))
+
+(defn multiplicand [p]
+  (last p))
+
+(defn deriv [exp var]
+  (cond (number? exp) 0
+        (variable? exp) (if (same-variable? exp var) 1 0)
+        (sum? exp) (make-sum (deriv (addend exp) var)
+                             (deriv (augend exp) var))
+        (product? exp) (make-sum (make-product (multiplier exp)
+                                               (deriv (multiplicand exp) var))
+                                 (make-product (deriv (multiplier exp) var)
+                                               (multiplicand exp)))
+        :else (throw (Exception. "unknown expression type -- DERIV" exp))))
+
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+(defn =number? [exp number]
+  (and (number? exp) (= exp number)))
+
+(defn make-sum [a1 a2]
+  (cond (=number? a1 0) a2
+        (=number? a2 0) a1
+        (and (number? a1) (number? a2)) (+ a1 a2)
+        :else (list '+ a1 a2)))
+
+(defn make-product [m1 m2]
+  (cond (or (=number? m1 0) (=number? m2 0)) 0
+        (=number? m1 1) m2
+        (=number? m2 1) m1
+        (and (number? m1) (number? m2)) (* m1 m2)
+        :else (list '* m1 m2)))
+
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
